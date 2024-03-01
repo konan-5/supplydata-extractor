a4c9baea957d54de5b315d275e7aae58c4722875
konan-5 <ikedahiroshi517@gmail.com>
2024-03-01 08:30:55 +0000

    Add scraping logic

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# import requests
-# from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 from dotenv import load_dotenv
-
+import time
 # Load environment variables
 load_dotenv()
 
@@ -17,23 +17,33 @@ client = gspread.authorize(credential)
 SHEET_URL = os.getenv("SHEET_URL")
 sheet = client.open_by_url(SHEET_URL).sheet1
 
-# Example data to input into the sheet
-# This is a list of lists, where each inner list represents a row,
-# and each element in the inner list represents a column value for that row.
-data_to_input = [
-    ["Name", "Age", "City"],  # First row: Column headers
-    ["Alice", 30, "New York"],  # Second row: Data
-    ["Bob", 25, "Los Angeles"],  # Third row: Data
-    # Add more rows as needed
-]
+file_path = 'urls.txt'
 
-# Loop through each row in the data
-for row_index, row in enumerate(data_to_input, start=1):  # start=1 because sheet rows and columns are 1-indexed
-    # Loop through each column value in the row
-    for col_index, value in enumerate(row, start=1):  # start=1 for the same reason
-        # Update the cell at the current row and column with the current value
-        sheet.update_cell(row_index, col_index, value)
+# Open the file and read its contents
+with open(file_path, 'r') as file:
+    # Loop through each line in the file
+    for idx, line in enumerate(file, start=1):  # start=1 begins the index from 1 instead of Python's default 0
+        # Each 'line' is a URL in this context, so you can process it here
+        url = line.strip() # Remove any leading/trailing whitespace
 
-# Print a message to indicate completion
-print("Data input complete.")
+        try:
+            resp = requests.get(url)
+            if resp.status_code == 200:
+                try:
+                    soup = BeautifulSoup(resp.content, features="html.parser")
+                    # Using 'string' instead of 'text' to comply with the updated BeautifulSoup syntax
+                    detailed_description = soup.find(string="Detailed description:").find_next().text.strip()
 
+                    awarded_supplier = soup.find(string="Awarded supplier:").find_next().text.strip()
+                    sheet.append_row([url,detailed_description,awarded_supplier])
+                except Exception as e:
+                    sheet.append_row([url,"",""])
+            else:
+                # Append a row with the URL and error messages if the status code is not 200
+                sheet.append_row([url,"",""])
+        except Exception as e:
+            # Append a row with the URL and exception message if an error occurs
+            sheet.append_row([url, "", ""])
+        finally :
+            time.sleep(5)
+            print(f"Done: {idx}")
